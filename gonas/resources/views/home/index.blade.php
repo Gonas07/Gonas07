3557db996913d4a1d76193c514b26f54190c2710
Gonas07 <139719923+Gonas07@users.noreply.github.com>
2023-08-04 17:45:54 +0000

    Pending changes exported from your codespace

---
+++ gonas/resources/views/home/index.blade.php
@@ -0,0 +1,228 @@
+@extends('layouts.app')
+
+@section('content')
+    <div class="home">
+        <h2>Gonas basic</h2>
+        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Modi mollitia neque voluptates voluptas quam quod eum,
+            ipsa
+            sunt iusto maxime.</p>
+        <div class="home__main-cards">
+            <div class="home__card">
+                <figure>
+                    <a href="#">
+                        <img src="https://picsum.photos/400/600" alt="Model name image">
+                    </a>
+                    <figcaption>
+                        <div>
+                            <h3>Model name</h3>
+                            <a href="#">El agustino</a>
+                        </div>
+                        <div>
+                            <a href="#">
+                                @include('icons.whatsapp')
+                            </a>
+                        </div>
+                    </figcaption>
+                </figure>
+            </div>
+            <div class="home__card">
+                <figure>
+                    <a href="#">
+                        <img src="https://picsum.photos/400/600" alt="Model name image">
+                    </a>
+                    <figcaption>
+                        <div>
+                            <h3>Model name</h3>
+                            <a href="#">El agustino</a>
+                        </div>
+                        <div>
+                            <a href="#">
+                                @include('icons.whatsapp')
+                            </a>
+                        </div>
+                    </figcaption>
+                </figure>
+            </div>
+            <div class="home__card">
+                <figure>
+                    <a href="#">
+                        <img src="https://picsum.photos/400/600" alt="Model name image">
+                    </a>
+                    <figcaption>
+                        <div>
+                            <h3>Model name</h3>
+                            <a href="#">El agustino</a>
+                        </div>
+                        <div>
+                            <a href="#">
+                                @include('icons.whatsapp')
+                            </a>
+                        </div>
+                    </figcaption>
+                </figure>
+            </div>
+            <div class="home__card">
+                <figure>
+                    <a href="#">
+                        <img src="https://picsum.photos/400/600" alt="Model name image">
+                    </a>
+                    <figcaption>
+                        <div>
+                            <h3>Model name</h3>
+                            <a href="#">El agustino</a>
+                        </div>
+                        <div>
+                            <a href="#">
+                                @include('icons.whatsapp')
+                            </a>
+                        </div>
+                    </figcaption>
+                </figure>
+            </div>
+            <div class="home__card">
+                <figure>
+                    <a href="#">
+                        <img src="https://picsum.photos/400/600" alt="Model name image">
+                    </a>
+                    <figcaption>
+                        <div>
+                            <h3>Model name</h3>
+                            <a href="#">El agustino</a>
+                        </div>
+                        <div>
+                            <a href="#">
+                                @include('icons.whatsapp')
+                            </a>
+                        </div>
+                    </figcaption>
+                </figure>
+            </div>
+            <div class="home__card">
+                <figure>
+                    <a href="#">
+                        <img src="https://picsum.photos/400/600" alt="Model name image">
+                    </a>
+                    <figcaption>
+                        <div>
+                            <h3>Model name</h3>
+                            <a href="#">El agustino</a>
+                        </div>
+                        <div>
+                            <a href="#">
+                                @include('icons.whatsapp')
+                            </a>
+                        </div>
+                    </figcaption>
+                </figure>
+            </div>
+            <div class="home__card">
+                <figure>
+                    <a href="#">
+                        <img src="https://picsum.photos/400/600" alt="Model name image">
+                    </a>
+                    <figcaption>
+                        <div>
+                            <h3>Model name</h3>
+                            <a href="#">El agustino</a>
+                        </div>
+                        <div>
+                            <a href="#">
+                                @include('icons.whatsapp')
+                            </a>
+                        </div>
+                    </figcaption>
+                </figure>
+            </div>
+            <div class="home__card">
+                <figure>
+                    <a href="#">
+                        <img src="https://picsum.photos/400/600" alt="Model name image">
+                    </a>
+                    <figcaption>
+                        <div>
+                            <h3>Model name</h3>
+                            <a href="#">El agustino</a>
+                        </div>
+                        <div>
+                            <a href="#">
+                                @include('icons.whatsapp')
+                            </a>
+                        </div>
+                    </figcaption>
+                </figure>
+            </div>
+            <div class="home__card">
+                <figure>
+                    <a href="#">
+                        <img src="https://picsum.photos/400/600" alt="Model name image">
+                    </a>
+                    <figcaption>
+                        <div>
+                            <h3>Model name</h3>
+                            <a href="#">El agustino</a>
+                        </div>
+                        <div>
+                            <a href="#">
+                                @include('icons.whatsapp')
+                            </a>
+                        </div>
+                    </figcaption>
+                </figure>
+            </div>
+            <div class="home__card">
+                <figure>
+                    <a href="#">
+                        <img src="https://picsum.photos/400/600" alt="Model name image">
+                    </a>
+                    <figcaption>
+                        <div>
+                            <h3>Model name</h3>
+                            <a href="#">El agustino</a>
+                        </div>
+                        <div>
+                            <a href="#">
+                                @include('icons.whatsapp')
+                            </a>
+                        </div>
+                    </figcaption>
+                </figure>
+            </div>
+            <div class="home__card">
+                <figure>
+                    <a href="#">
+                        <img src="https://picsum.photos/400/600" alt="Model name image">
+                    </a>
+                    <figcaption>
+                        <div>
+                            <h3>Model name</h3>
+                            <a href="#">El agustino</a>
+                        </div>
+                        <div>
+                            <a href="#">
+                                @include('icons.whatsapp')
+                            </a>
+                        </div>
+                    </figcaption>
+                </figure>
+            </div>
+            <div class="home__card">
+                <figure>
+                    <a href="#">
+                        <img src="https://picsum.photos/400/600" alt="Model name image">
+                    </a>
+                    <figcaption>
+                        <div>
+                            <h3>Model name</h3>
+                            <a href="#">El agustino</a>
+                        </div>
+                        <div>
+                            <a href="#">
+                                @include('icons.whatsapp')
+                            </a>
+                        </div>
+                    </figcaption>
+                </figure>
+            </div>
+        </div>
+    </div>
+@endsection
---
+++ gonas/resources/views/sidebars/main.blade.php
@@ -0,0 +1,150 @@
+<aside class="sidebar">
+    <div class="sidebar__container">
+        <div class="sidebar__option">
+            <h4>
+                Buscar nombre
+            </h4>
+            <div class="sidebar__option-hide">
+                <div class="sidebar__option-input">
+                    <input type="text" placeholder="Buscar nombre">
+                </div>
+            </div>
+        </div>
+        <div class="sidebar__option">
+            <h4>
+                Ciudad
+            </h4>
+            <div class="sidebar__option-hide">
+                <div class="sidebar__option-input">
+                    <select name="" id="">
+                        <option value="" disabled selected>Seleccione una ciudad</option>
+                        <option value="lima">Lima</option>
+                        <option value="arequipa">Arequipa</option>
+                        <option value="cusco">Cusco</option>
+                    </select>
+                </div>
+            </div>
+        </div>
+        <div class="sidebar__option">
+            <h4>
+                Rango de precio
+            </h4>
+            <div class="sidebar__option-hide">
+                <div class="sidebar__option-price">
+                    <div class="sidebar__option-input">
+                        <span>S/.</span>
+                        <input type="number" placeholder="Min">
+                    </div>
+                    <span></span>
+                    <div class="sidebar__option-input">
+                        <span>S/.</span>
+                        <input type="number" placeholder="Max">
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div class="sidebar__option">
+            <h4>
+                Caracteristicas
+            </h4>
+            <div class="sidebar__option-hide">
+                <ul>
+                    <li>
+                        <input type="checkbox" name="" id="">
+                        <label for="">Caracteristica 1</label>
+                    </li>
+                    <li>
+                        <input type="checkbox" name="" id="">
+                        <label for="">Caracteristica 2</label>
+                    </li>
+                    <li>
+                        <input type="checkbox" name="" id="">
+                        <label for="">Caracteristica 3</label>
+                    </li>
+                    <li>
+                        <input type="checkbox" name="" id="">
+                        <label for="">Caracteristica 4</label>
+                    </li>
+                    <li>
+                        <input type="checkbox" name="" id="">
+                        <label for="">Caracteristica 5</label>
+                    </li>
+                    <li>
+                        <input type="checkbox" name="" id="">
+                        <label for="">Caracteristica 6</label>
+                    </li>
+                </ul>
+            </div>
+        </div>
+        <div class="sidebar__option">
+            <h4>
+                Servicios
+            </h4>
+            <div class="sidebar__option-hide">
+                <ul>
+                    <li>
+                        <input type="checkbox" name="" id="">
+                        <label for="">Trato de pareja</label>
+                    </li>
+                    <li>
+                        <input type="checkbox" name="" id="">
+                        <label for="">Oral con condon</label>
+                    </li>
+                    <li>
+                        <input type="checkbox" name="" id="">
+                        <label for="">Oral sin condon</label>
+                    </li>
+                    <li>
+                        <input type="checkbox" name="" id="">
+                        <label for="">Anal</label>
+                    </li>
+                    <li>
+                        <input type="checkbox" name="" id="">
+                        <label for="">Garganta profunda</label>
+                    </li>
+                </ul>
+            </div>
+        </div>
+        <div class="sidebar__option">
+            <h4>
+                Servicios Especiales
+            </h4>
+            <div class="sidebar__option-hide">
+                <ul>
+                    <li>
+                        <input type="checkbox" name="" id="">
+                        <label for="">Atencion a parejas</label>
+                    </li>
+                    <li>
+                        <input type="checkbox" name="" id="">
+                        <label for="">Despedida de soltero</label>
+                    </li>
+                </ul>
+            </div>
+        </div>
+        <div class="sidebar__option">
+            <h4>Tipo de anunciante</h4>
+            <div class="sidebar__option-hide">
+                <ul>
+                    <li>
+                        <input type="checkbox" name="" id="">
+                        <label for="">Verificadas</label>
+                    </li>
+                    <li>
+                        <input type="checkbox" name="" id="">
+                        <label for="">Exclusivas</label>
+                    </li>
+                    <li>
+                        <input type="checkbox" name="" id="">
+                        <label for="">Premium</label>
+                    </li>
+                    <li>
+                        <input type="checkbox" name="" id="">
+                        <label for="">Basic</label>
+                    </li>
+                </ul>
+            </div>
+        </div>
+    </div>
+    <span class="sidebar__bottom-gradient"></span>
+</aside>
